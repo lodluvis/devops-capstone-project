9831808e12968e80f09b4f6cb24149657eff3662
lodluvis <hieudao.cloud@gmail.com>
2023-05-23 02:15:52 -0400

    fix 3

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -171,4 +171,4 @@ class TestAccountService(TestCase):
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
-        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)S
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
